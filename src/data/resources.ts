c15bacd3647e7d106b21863a32975aaf696eca5b
Steve <stevedsimkins@gmail.com>
2025-05-02 16:17:12 -0400

    chore: Refactored data

---
+++ src/data/resources.ts
@@ -0,0 +1,73 @@
+export const ideas = [
+  {
+    title: "What is Jamstack?",
+    url: "https://whitep4nth3r.com/blog/what-is-jamstack/"
+  },
+  {
+    title: "Why Jamstack?",
+    url: "https://jamstack.org/why-jamstack/"
+  },
+  {
+    title: "The Static Website Manifesto",
+    url: "https://orbiter.host/blog/the-static-website-manifesto"
+  },
+  {
+    title: "Jamstack: Why and How to Get Started",
+    url: "https://snipcart.com/blog/jamstack"
+  },
+  {
+    title: "A Case for Local Builds",
+    url: "https://orbiter.host/blog/a-case-for-local-builds"
+  },
+  {
+    title: "Is Jamstack Finished?",
+    url: "https://remotesynthesis.com/blog/goodbye-jamstack/"
+  },
+  {
+    title: "The Open Web and What it Actually Means",
+    url: "https://orbiter.host/blog/the-open-web-and-what-it-actually-means"
+  },
+  {
+    title: "The Future of Jamstack is Anti-Capitalist",
+    url: "https://whitep4nth3r.com/blog/the-future-of-jamstack-is-anti-capitalist/"
+  },
+  {
+    title: "Jamstack and Mobile Apps",
+    url: "https://youtube.com/shorts/i4tDJfKmy4w"
+  },
+  {
+    title: "Let's not rewrite websites",
+    url: "https://youtube.com/shorts/umN9nWjbh-U"
+  },
+  {
+    title: "Stop Paying for Build Minutes",
+    url: "https://orbiter.host/blog/stop-paying-for-build-minutes"
+  },
+  {
+    title: "The Web Was Meant for Surfing not Scrolling",
+    url: "https://orbiter.host/blog/the-web-was-meant-for-surfing-not-scrolling"
+  }
+];
+
+export const tutorials = [
+  {
+    title: "How to Convert Your Wordpress Site into a Static Site",
+    url: "https://orbiter.host/blog/how-to-convert-your-wordpress-site-into-a-static-site"
+  },
+  {
+    title: "How to Build and Host an 11ty Site",
+    url: "https://orbiter.host/blog/how-to-build-and-host-an-11ty-site"
+  },
+  {
+    title: "How to Host a React Site",
+    url: "https://orbiter.host/blog/how-to-host-a-react-site"
+  },
+  {
+    title: "How to Host an Astro Site",
+    url: "https://orbiter.host/blog/how-to-host-an-astro-site"
+  },
+  {
+    title: "How to Build and Host a Hugo Site",
+    url: "https://orbiter.host/blog/how-to-build-and-host-a-hugo-site"
+  }
+];
